f940b7c3712823e35728993d19f5ed26242f14b5
Nehemiah Cheruiyot <nehemiahcheruiyot7069@gmail.com>
2023-12-16 22:49:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// index.js
+
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Export the functions to make them accessible in other files
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
